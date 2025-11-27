f54fba4244c2f644db618001b338e88f4adf8aae
hagar_marzouk <148659348+hagarMarzouk99@users.noreply.github.com>
2025-11-27 18:50:31 +0200

    Update task2_1.asm

--- task_lab2/task2_1.asm
+++ task_lab2/task2_1.asm
@@ -1,55 +1,52 @@
 .data 
-prompt : .asciiz "Enter number of integers : "
-promptNumber : .asciiz "Enter an integer : "
+prompt: .asciiz "Enter number of integers : "
+promptNumber: .asciiz "Enter an integer : "
 result: .asciiz "the average is : "
-newline : .asciiz "\n"
+newline: .asciiz "\n"
 
 .text 
 main:
-move $t0 , $zero    # set inital value of sum
-move $t2 , $zero     # counter = 0 "at start"
-#print prompt 
-li $v0,4
-la $a0 , prompt
+move $t0, $zero
+move $t2, $zero
+
+li $v0, 4
+la $a0, prompt
 syscall
 
-# read number 
-li $v0,5
+li $v0, 5
 syscall
-move $t1 , $v0     #   t1 = n = number of integer
+move $t1, $v0
 
 while:
-bge $t2 , $t1, endWhile
-# print prompt number
-   li $v0, 4
-    la $a0, enterNum
-    syscall
-    li $v0, 5  # read an integer
-    syscall
-# operation (add)
-  add $t0, $t0, $v0   # => sum += number
- addi $t2, $t2, 1   # to increament the counter
- j while
- endwhile:
-# average
- div $t0, $t1
- mflo $t3    # t3 ===> result 
+bge $t2, $t1, endwhile
 
-# to print the result
- li $v0, 4
- la $a0, result
- syscall
-# 
-li $v0 ,1
-move $a0 , $t3
+li $v0, 4
+la $a0, promptNumber
 syscall
 
-#new line 
-    li $v0, 4
-    la $a0, newline
-    syscall
-
-exit:
-li $v0 , 10
+li $v0, 5
 syscall
+
+add $t0, $t0, $v0
+addi $t2, $t2, 1
+j while
+
+endwhile:
+div $t0, $t1
+mflo $t3
+
+li $v0, 4
+la $a0, result
+syscall
+
+li $v0, 1
+move $a0, $t3
+syscall
+
+li $v0, 4
+la $a0, newline
+syscall
+
+li $v0, 10
+syscall 
 
